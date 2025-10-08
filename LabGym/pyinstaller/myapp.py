b3d6474e99a673ed358b2e07e626a00c360fd406
Bobby Tomlinson <btomlinson237@gmail.com>
2025-10-08 11:09:05 -0400

    Moving multiprocessing setup and import of main to prevent child process from relaunching GUI

--- LabGym/pyinstaller/myapp.py
+++ LabGym/pyinstaller/myapp.py
@@ -8,19 +8,16 @@ import re
 import sys
 import os
 
-# Multiprocessing start method is spawn before imports; child processes won't inherit main process's state
-if sys.platform == "darwin":
-    multiprocessing.set_start_method('spawn', force=True)
-
-# free_zupport() on macOS prevents double-launch issues
-if sys.platform == "darwin":
-    multiprocessing.freeze_support()
-
-
-# Once the multiprocessing is established, the main function can be imported and executed
-from LabGym.__main__ import main
-
 if __name__ == "__main__":
-    # argv[0] would be nicer if launched via a script wrapper
+    import multiprocessing as mp
+    mp.freeze_support()
+    try:
+        mp.set_start_method("spawn", force=True)
+    except RuntimeError:
+        pass
+
+    # Importing app entry after the start method has been set
+    from LabGym.__main__ import main
+
     sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
     sys.exit(main())
