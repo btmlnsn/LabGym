1350e7a60fbe04912cc705875bdd197f56c27277
Bobby Tomlinson <btomlinson237@gmail.com>
2026-01-07 15:26:19 -0500

    rearch(move categorizer): removing deprecated Categorizers() import (module name has changed)

--- LabGym/core/__init__.py
+++ LabGym/core/__init__.py
@@ -1,10 +1,7 @@
 # Core modules
 from .analyzebehavior import AnalyzeAnimal
 from .analyzebehavior_dt import AnalyzeAnimalDetector
-from .categorizer import Categorizers
-
 __all__ = [
     'AnalyzeAnimal',
     'AnalyzeAnimalDetector',
-    'Categorizers',
 ]
